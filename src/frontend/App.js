677e3290fcd313b2c73e6a5c6bd723a04bb43c5a
Gaby karam <gaby@codi.tech>
2019-08-26 16:53:44 +0300

    adding dahsboard v1

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PageIntro from './pages/PageIntro';
 import PageStudent from './pages/PageStudent';
+import App from './admin/index';
 import './App.css';
 
-const App = () => (
+const AdminApp = () => (
   <div>
     <Switch>
       <Route exact path="/Home" component={PageStudent} />
       <Route exact path="/" component={PageIntro} />
+      <Route exact path="/admin" component={App} />
     </Switch>
   </div>
 );
 
-export default App;
+export default AdminApp;
